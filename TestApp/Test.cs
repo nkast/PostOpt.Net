eaa51fabec18558e0fc68156be7988df38aac5fa
Nikos Kastellanos <nkastellanos@gmail.com>
2019-02-05 02:17:14 +0200

    Add TestMethodAddOp(val, ref,  out) and reference TestMethodAdd_vro(...)

--- TestApp/Test.cs
+++ TestApp/Test.cs
@@ -88,6 +88,17 @@ namespace testApp
             sw.Start();
             for (int c = 0; c < 10000000; c++)
             {
+                TestMethodAddOp(a, ref b, out r);
+            }
+            sw.Stop();
+            System.Diagnostics.Trace.WriteLine("TestMethodAddOp(v,ref v,out v): " + sw.Elapsed.TotalMilliseconds+"ms");
+            System.Console.WriteLine("TestMethodAddOp(v,ref v,out v): " + sw.Elapsed.TotalMilliseconds+"ms");
+
+            sw.Reset();
+            
+            sw.Start();
+            for (int c = 0; c < 10000000; c++)
+            {
                 r = TestMethodAddRefs();
             }
             sw.Stop();
@@ -104,6 +115,17 @@ namespace testApp
             sw.Stop();
             System.Diagnostics.Trace.WriteLine("TestMethodAdd_vrv(): " + sw.Elapsed.TotalMilliseconds+"ms");
             System.Console.WriteLine("TestMethodAdd_vrv(): " + sw.Elapsed.TotalMilliseconds+"ms");
+
+            
+            sw.Start();
+            for (int c = 0; c < 10000000; c++)
+            {
+                TestMethodAddOp(a, ref b, out r);
+            }
+            sw.Stop();
+            //System.Diagnostics.Trace.WriteLine("TestMethodAdd_vrv(): " + sw.Elapsed.TotalMilliseconds+"ms");
+            //System.Console.WriteLine("TestMethodAdd_vrv(): " + sw.Elapsed.TotalMilliseconds+"ms");
+
 
             var ra = r;
         }
@@ -195,11 +217,21 @@ namespace testApp
             return r;
         }
 
+        public static void TestMethodAddOp(Vector2 a, ref Vector2 b, out Vector2 r)
+        {
+            r = a + b;
+        }
+
         public static Vector2 TestMethodAdd_vrv(Vector2 a, ref Vector2 b)
         {
             Vector2 r;
             r = Vector2.Add(a, ref b);
             return r;
+        }
+
+        public static void TestMethodAdd_vro(Vector2 a, ref Vector2 b, out Vector2 r)
+        {            
+            Vector2.Add(a, ref b, out r);
         }
     }
 }
