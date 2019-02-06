f39ff1ac6de92c1d59a64f6005a26e0da08075f0
Nikos Kastellanos <nkastellanos@gmail.com>
2019-02-06 03:57:51 +0200

    Update Branches Target

--- PostOpt/ILProcessorEx.cs
+++ PostOpt/ILProcessorEx.cs
@@ -32,14 +32,43 @@ namespace PostOpt
         {
             for (var instruction = this.Processor.Body.Instructions[0]; instruction != null; instruction = instruction.Next)
             {
-                if (instruction.OpCode == OpCodes.Blt ||
-                    instruction.OpCode == OpCodes.Blt_S
-                    )
+                if (instruction.OpCode == OpCodes.Beq
+                 || instruction.OpCode == OpCodes.Beq_S
+                 || instruction.OpCode == OpCodes.Bge
+                 || instruction.OpCode == OpCodes.Bge_S
+                 || instruction.OpCode == OpCodes.Bge_Un
+                 || instruction.OpCode == OpCodes.Bge_Un_S
+                 || instruction.OpCode == OpCodes.Bgt
+                 || instruction.OpCode == OpCodes.Bgt_S
+                 || instruction.OpCode == OpCodes.Bgt_Un
+                 || instruction.OpCode == OpCodes.Bgt_Un_S
+                 || instruction.OpCode == OpCodes.Ble
+                 || instruction.OpCode == OpCodes.Ble_S
+                 || instruction.OpCode == OpCodes.Ble_Un
+                 || instruction.OpCode == OpCodes.Ble_Un_S
+                 || instruction.OpCode == OpCodes.Blt
+                 || instruction.OpCode == OpCodes.Blt_S
+                 || instruction.OpCode == OpCodes.Blt_Un
+                 || instruction.OpCode == OpCodes.Blt_Un_S
+                 || instruction.OpCode == OpCodes.Bne_Un
+                 || instruction.OpCode == OpCodes.Bne_Un_S                 
+                 || instruction.OpCode == OpCodes.Br
+                 || instruction.OpCode == OpCodes.Br_S
+                 || instruction.OpCode == OpCodes.Brfalse
+                 || instruction.OpCode == OpCodes.Brfalse_S
+                 || instruction.OpCode == OpCodes.Brtrue
+                 || instruction.OpCode == OpCodes.Brtrue_S)
                 {
                     if (instruction.Operand == oldTarget)
                     {
                         instruction.Operand = newTarget;
                     }
+                }
+
+                if (instruction.OpCode == OpCodes.Switch)
+                {
+                    //TODO: update switch branch target 
+                    throw new NotImplementedException("Patching Switch branch targets in not Implemented");
                 }
             }
             return;
